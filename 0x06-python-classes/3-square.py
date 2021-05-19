d096813e927f10243c0e93e47e143ad5d5abf4de
ssebastiancardona <ssebastiancardona@gmail.com>
2021-05-18 22:23:41 -0500

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Square Module - Use when you need to print a square
+"""
+
+
+class Square:
+    """
+    Class defining the size of a square
+    """
+    def __init__(self, size=0):
+        """
+        Initialization of instanced attribute
+        Args:
+        size: The size of a square, it must be an integer and >= 0
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square
+        Returns the calculated area
+        """
+        return self.__size * self.__size
