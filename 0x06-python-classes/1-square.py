a8f1ba88a4582911352f9b13dfe608789db975b1
ssebastiancardona <ssebastiancardona@gmail.com>
2021-05-18 21:58:36 -0500

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Square Module - Use when you need to print a square
+"""
+
+
+class Square:
+    """
+    Class defining the size of a square
+    """
+    def __init__(self, size=0):
+        """
+        Initialization of instanced attribute
+        Args:
+        size: The size of a square
+        """
+        self.__size = size
