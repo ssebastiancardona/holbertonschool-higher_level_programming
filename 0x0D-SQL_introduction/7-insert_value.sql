e15b6e71327bb8c802a41bd81ffa79d1884a8843
ssebastiancardona <ssebastiancardona@gmail.com>
2021-07-12 22:55:59 -0500

    7-insert_value.sql

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,7 @@
+-- inserta una nueva fila en la tabla first_table (base de datos hbtn_0c_0) en su servidor MySQL
+-- Nueva fila:
+-- id = 89
+-- nombre = Escuela Holberton
+-- El nombre de la base de datos se pasará como argumento del comando mysql
+
+INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Holberton School');
