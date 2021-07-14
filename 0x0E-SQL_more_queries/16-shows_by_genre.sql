3b7ccd70932e2795cc0301f4a5f77035e7b4da31
ssebastiancardona <ssebastiancardona@gmail.com>
2021-07-13 23:48:16 -0500

    16-shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- enumera todos los programas, y todos los géneros vinculados a ese programa, de la base de datos hbtn_0d_tvshows.
+-- Si un programa no tiene un género, muestra NULL en la columna de género.
+-- Cada registro debe mostrar: tv_shows.title - tv_genres.name
+-- Los resultados deben ordenarse en orden ascendente por el título del programa y el nombre del género.
+-- Puede usar solo una instrucción SELECT
+-- El nombre de la base de datos se pasará como argumento del comando mysql
