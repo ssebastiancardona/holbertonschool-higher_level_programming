c321f5e2a96ae8a0947e8340096e48c565a28437
ssebastiancardona <ssebastiancardona@gmail.com>
2021-08-03 21:19:53 -0500

    8-esrever.js

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// Function that returns the reversed version of a list
+exports.esrever = function (list) {
+    const myList = [];
+    for (let i = list.length - 1; i >= 0; i--) {
+      myList.push(list[i]);
+    }
+    return (myList);
+  };
