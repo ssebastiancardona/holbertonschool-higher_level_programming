d11962f95ba0be499e352aaa8990e0a2c8c18249
ssebastiancardona <ssebastiancardona@gmail.com>
2021-08-19 23:05:59 -0500

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 # toma una URL, envía una solicitud POST a la URL pasada, muestra el cuerpo de la respuesta
+curl "$1" -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
