7976906d1bd3deef5055bc875186c51844660659
ssebastiancardona <ssebastiancardona@gmail.com>
2021-08-19 22:58:58 -0500

    4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 # Escriba un script Bash que tome una URL como argumento, envíe una solicitud GET a la URL y muestre el cuerpo de la respuesta
+curl "$1" -sX GET -H "X-HolbertonSchool-User-Id:98"
